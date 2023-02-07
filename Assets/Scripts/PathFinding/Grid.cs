1fb3f71299fc7fad1bc7cf29b1ad38ad32c6828c
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-02-07 18:06:45 +0100

    l

---
+++ Assets/Scripts/PathFinding/Grid.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Script
+{
+    public class Grid : MonoBehaviour
+    {
+        [SerializeField] private GameObject ground;
+        [SerializeField] private GameObject debugging;
+
+        [Range(1, 32)] [SerializeField] private int amountOfTiles;
+        private int tempAmountOfTiles;
+        
+        public static NodeBase[,] grid = new NodeBase[10000, 10000];
+        
+        private List<GameObject> listOfDebugging = new ();
+
+        private NodeBase startNode;
+        private NodeBase endNode;
+        
+        private void Start()
+        {
+            GenerateGrid();
+        }
+
+        private void Update()
+        {
+            if (amountOfTiles != tempAmountOfTiles) GenerateGrid();
+        }
+
+        private void GenerateGrid()
+        {
+            foreach (var obj in listOfDebugging)
+            {
+                Destroy(obj);
+            }
+            tempAmountOfTiles = amountOfTiles;
+            var bounds = ground.GetComponent<Collider>().bounds;
+            for (float x = 0; x < bounds.max.x - bounds.min.x; x += 1f / amountOfTiles)
+            {
+                for (float z = 0; z < bounds.max.z - bounds.min.z; z += 1f / amountOfTiles)
+                {
+                    //CreateChildren(new Vector2(bounds.min.x + x, bounds.min.z + z));
+                    
+                    var node = new NodeBase
+                    {
+                        position = new Vector3(x, 0, z)
+                    };
+                    
+                    grid[(int) x * amountOfTiles, (int) z * amountOfTiles] = node;
+
+                    Instantiate(debugging, node.position, Quaternion.identity);
+                }
+            }
+        }
+        
+        private void CreateChildren(Vector2 pos)
+        {
+            var increment = 1f / amountOfTiles;
+            print(increment);
+
+            var node = new NodeBase
+            {
+                children =
+                {
+                    
+                    new NodeBase
+                    {
+                        position = new Vector3(pos.x - increment, 0, pos.y - increment)
+                    },
+                    new NodeBase
+                    {
+                        position = new Vector3(pos.x + increment, 0, pos.y - increment)
+                    },
+                    new NodeBase
+                    {
+                        position = new Vector3(pos.x - increment, 0, pos.y + increment)
+                    }
+                }
+            };
+            
+            grid[(int) pos.x, (int) pos.y] = node;
+
+            foreach (var child in node.children)
+            {
+                Instantiate(debugging, child.position, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathFinding/PathFinding.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PathFinding : MonoBehaviour
+{
+    private static Color startColor = Color.green;
+    private static Color endColor = Color.red;
+    private static Color pathColor = Color.cyan;
+    private static Color pathSwamp = Color.blue;
+    private static Color visitedColor = Color.magenta;
+
+    public static List<NodeBase> FindPath(NodeBase startNode, NodeBase endNode)
+    {
+        print("lol");
+        var toVisit = new List<NodeBase> { startNode };
+        var visited = new List<NodeBase>();
+
+        //Loops while there are nodes to visit
+        while (toVisit.Any())
+        {
+            print("toVisit: " + toVisit.Count);
+            var current = toVisit[0];
+
+            foreach (var node in toVisit)
+                if (node.F < current.F || node.F == current.F && node.H < current.H) current = node;
+            
+            visited.Add(current);
+
+            current.obj.GetComponent<SpriteRenderer>().color = visitedColor;
+            SetCosts(current);
+            
+            toVisit.Remove(current);
+
+            //If end node is reached, return path
+            if (current == endNode)
+            {
+                print("EndNode Reached");
+                var currentPathTile = endNode;
+                var path = new List<NodeBase>();
+                var count = 1000;
+                
+                //Loops while there are still tiles in the path
+                while (currentPathTile != startNode) {
+                    path.Add(currentPathTile);
+                    if (currentPathTile == endNode) currentPathTile.obj.GetComponent<SpriteRenderer>().color = endColor;
+                    else currentPathTile.obj.GetComponent<SpriteRenderer>().color = currentPathTile.costMultiplyer == 2 ? pathSwamp : pathColor;
+
+                    currentPathTile = currentPathTile.connectedNode;
+
+                    count--;
+                    if (count < 0) throw new Exception();
+                    print("Backtracking");
+                }
+
+                currentPathTile.obj.GetComponent<SpriteRenderer>().color = startColor;
+                
+                Debug.Log(path.Count);
+                return path;
+            }
+
+            //Loops through all neighbours of current node
+            foreach (var neighbor in current.GetNeighbors().Where(t => t.walkable && !visited.Contains(t)))
+            {
+                print("Neibors: " + current.GetNeighbors().Count);
+                var inSearch = toVisit.Contains(neighbor);
+                var costToNeighbor = current.G + current.GetDistance(neighbor);
+                
+                //If the cost to the neighbor is lower than the current cost, set the new cost and set the current node as the connected node
+                if (!inSearch || costToNeighbor < neighbor.G) {
+                    print("First");
+                    neighbor.SetG(costToNeighbor);
+                    neighbor.SetConnection(current);
+
+                    //If the neighbor is not in the search list, add it
+                    if (!inSearch) {
+                        print("Second");
+                        neighbor.SetH(neighbor.GetDistance(endNode));
+                        toVisit.Add(neighbor);
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static void SetCosts(NodeBase node)
+    {
+        FindChild(node.obj, "G").GetComponent<TMP_Text>().text = Convert.ToInt32(node.G * 10).ToString();
+        FindChild(node.obj, "H").GetComponent<TMP_Text>().text = Convert.ToInt32(node.H * 10).ToString();
+        FindChild(node.obj, "F").GetComponent<TMP_Text>().text = Convert.ToInt32(node.F * 10).ToString();
+    }
+
+    private static GameObject FindChild(GameObject obj, string name)
+    {
+        for (int i = 0; i < obj.transform.childCount; i++)
+        {
+            if (obj.transform.GetChild(i).name == name) return obj.transform.GetChild(i).gameObject;
+        }
+
+        return null;
+    }
+}
