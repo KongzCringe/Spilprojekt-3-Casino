8771bc844de90fdbf4240809fba407404a769ffc
leonardzz7 <leonard@leooli.dk>
2023-02-08 23:40:03 +0100

    hsj

--- Assets/MoveMouseScript.cs
+++ Assets/MoveMouseScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MoveMouseScript : MonoBehaviour
 {
-    bool pickedUp = false;
+    [SerializeField] bool pickedUp = false;
     // Start is called before the first frame update
     void Start()
     {
