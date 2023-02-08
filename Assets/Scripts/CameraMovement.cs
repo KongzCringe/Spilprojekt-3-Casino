2e7eedb50506dbefbf9c7b2f4f278a32e82089c0
Jasper Fog <Jasp3r@live.dk>
2023-02-07 17:34:41 -0800

    cameramovement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float smoothFactor = 0.5f;
+    public Vector3 minBoundaries = new Vector3(-10, -10, -10);
+    public Vector3 maxBoundaries = new Vector3(10, 10, 10);
+    public float boundarySmoothing = 0.1f;
+
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        targetPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forwards = transform.forward * vertical;
+        Vector3 sideways = transform.right * horizontal;
+
+        targetPosition += (forwards + sideways) * speed * Time.deltaTime;
+        targetPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x, minBoundaries.x, maxBoundaries.x),
+            Mathf.Clamp(targetPosition.y, minBoundaries.y, maxBoundaries.y),
+            Mathf.Clamp(targetPosition.z, minBoundaries.z, maxBoundaries.z)
+        );
+
+        Vector3 clampedTarget = targetPosition;
+        Vector3 currentPosition = transform.position;
+        Vector3 smoothedTarget = Vector3.Lerp(currentPosition, clampedTarget, boundarySmoothing);
+
+        if ((clampedTarget - currentPosition).magnitude < (smoothedTarget - currentPosition).magnitude)
+        {
+            transform.position = clampedTarget;
+        }
+        else
+        {
+            transform.position = smoothedTarget;
+        }
+    }
+}
