cfa4948819849f62f58cfdf7632237736304a414
Jasper Fog <Jasp3r@live.dk>
2023-02-09 10:12:25 -0800

    aggada

---
+++ Assets/export/CarSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject[] Cars;
+    public int maxRange;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRandomObject());
+    }
+
+    IEnumerator SpawnRandomObject()
+    {
+        while (true)
+        {
+            int whichCar = Random.Range (0, maxRange);
+        
+            GameObject myObj = Instantiate (Cars [whichCar],transform.position, transform.rotation * Quaternion.Euler(0f,0f,0f )) as GameObject;
+
+            myObj.transform.position = transform.position;
+            
+        
+            yield return new WaitForSeconds(Random.Range(4, 6));
+            
+        }
+    }
+}
