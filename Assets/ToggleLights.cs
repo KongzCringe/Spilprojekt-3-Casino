3d8b851a8cd33876e75b3033c04c2f7be73dd566
Jasper Fog <Jasp3r@live.dk>
2023-02-15 14:04:00 -0800

    fasda

--- Assets/ToggleLights.cs
+++ Assets/ToggleLights.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -8,26 +9,48 @@ public class ToggleLights : MonoBehaviour
     [SerializeField] private GameObject GreenScreen1;
     [SerializeField] private GameObject GreenScreen2;
     [SerializeField] private GameObject GreenScreen3;
+    
+    [SerializeField] private GameObject PivotLights;
 
+    private bool Activated;
+    
     void Start()
     {
         Flicker();
+    }
+
+    private void Update()
+    {
+        if (Activated)
+        {
+            PivotLights.transform.Rotate (0,0,50 * Time.deltaTime * 5); //rotates 50 degrees per second around z axis
+        }
     }
 
     public void Flicker()
     {
         StartCoroutine(FlickerCoroutine());
         StartCoroutine(FlickerScreen());
+
+        Activated = true;
     }
 
     private IEnumerator FlickerCoroutine()
     {
+        PivotLights.SetActive(!PivotLights.activeSelf);
+        
+        
+
         for (int i = 0; i < 10; i++)
         {
             lightComponent.SetActive(!lightComponent.activeSelf);
             
             yield return new WaitForSeconds(0.5f);
         }
+        
+        PivotLights.SetActive(!PivotLights.activeSelf);
+
+        Activated = false;
     }
 
     private IEnumerator FlickerScreen()
