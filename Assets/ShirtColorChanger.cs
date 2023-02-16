a346838d0cb1ad31f79da9ff49fc23ff0c0c995b
Jasper Fog <Jasp3r@live.dk>
2023-02-16 14:36:32 -0800

    dsfe

--- Assets/ShirtColorChanger.cs
+++ Assets/ShirtColorChanger.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class ShirtColorChanger : MonoBehaviour
 {
+    [SerializeField] private GameObject[] HatList;
+    [SerializeField] private GameObject[] BrilleList;
+
+    private int randomHat;
+    private int randomBrille;
+    
     // Start is called before the first frame update
     void Start()
     {
+        randomBrille = Random.Range(0, BrilleList.Length);
+        randomHat = Random.Range(0, HatList.Length);
+        
+        HatList[randomHat].SetActive(true);
+        BrilleList[randomBrille].SetActive(true);
+        
         GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
     }
-
-   
 }
