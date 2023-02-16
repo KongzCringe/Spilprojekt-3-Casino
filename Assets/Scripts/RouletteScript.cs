e3e1db511a3c2112e6d07ba2fea156e1848add54
leonardzz7 <leonard@leooli.dk>
2023-02-16 18:15:46 +0100

    kjjhjg

--- Assets/Scripts/RouletteScript.cs
+++ Assets/Scripts/RouletteScript.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class RouletteScript : MonoBehaviour
 {
+    [SerializeField] public int bet;
+    public int machineMoney;
     GameObject winChance;
     Slider winSlider;
     // Start is called before the first frame update
@@ -22,6 +24,17 @@ public class RouletteScript : MonoBehaviour
 
     public void RouletteFunction()
     {
+        int win = Random.Range(1, 101);
+        int thisBet = (Random.Range(bet, (bet * 2 + 1)));
+        if (win < winSlider.value)
+        {
+            machineMoney -= thisBet;
 
-    }
+        }
+        else
+        {
+            machineMoney += ((thisBet) * 10);
+        }
+
+        }
 }
