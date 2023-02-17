fbeda758e886726789659ead274cd24cccb156d5
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-02-17 20:20:29 +0100

    q

--- Assets/FadeInButtons.cs
+++ Assets/FadeInButtons.cs
@@ -13,39 +13,45 @@ public class FadeInButtons : MonoBehaviour
     private TMP_Text playText;
     private TMP_Text settingsText;
 
+    private Animation playAnim;
+    private Animation settingsAnim;
+
+    private bool hasStarted;
+
     private void Start()
     {
         playBtn.GetComponent<Button>().enabled = false;
-        settingsBtn.GetComponent<Button>().enabled = false;
+        //settingsBtn.GetComponent<Button>().enabled = false;
 
         playText = playBtn.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
-        settingsText = settingsBtn.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
+        //settingsText = settingsBtn.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
+
+        playAnim = playText.GetComponent<Animation>();
+        //settingsAnim = settingsText.GetComponent<Animation>();
 
         var startColor = playText.color;
         startColor.a = 0;
 
         playText.color = startColor;
-        settingsText.color = startColor;
+        //settingsText.color = startColor;
     }
-    
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) StartCoroutine(FadeIn());
+        if (Input.GetKeyDown(KeyCode.Space)) FadeIn();
+
+        if (!playAnim.isPlaying && hasStarted)
+        {
+            playBtn.GetComponent<Button>().enabled = true;
+            //settingsBtn.GetComponent<Button>().enabled = true;
+        }
     }
 
-    IEnumerator FadeIn()
+    private void FadeIn()
     {
-        var color = playText.color;
-        color.a = 0;
-        
-        while (playText.color.a < 255 && settingsText.color.a < 255)
-        {
-            color.a += 5;
+        playAnim.Play();
+        //settingsAnim.Play();
 
-            playText.color = color;
-            settingsText.color = color;
-
-            yield return new WaitForSeconds(0.05f);
-        }
+        hasStarted = true;
     }
 }
