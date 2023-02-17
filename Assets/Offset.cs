2e50483c57836d99611cd56489c68872cabbc0f5
Daniel From Kjærgaard <KongzCringe@gmail.com>
2023-02-17 18:19:51 +0100

    q

---
+++ Assets/Offset.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Offset : MonoBehaviour
+{
+    void Start()
+    {
+        var bounds = GetComponent<Collider>().bounds;
+
+        transform.position += new Vector3(bounds.extents.x, 0, bounds.extents.z);
+    }
+}
